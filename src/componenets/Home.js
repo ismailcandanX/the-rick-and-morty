2ce200387463913824775eae0e75e02fd2c4baf2
ismailcandanx <ismailcandan44@gmail.com>
2022-07-01 03:26:42 +0300

    Added some css code .

--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -2,17 +2,18 @@ import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getEpisodes } from '../actions'
-
+import "./Home.css"
 function Home(props) {
 
     useEffect(() => {
         props.getEpisodes()
     }, [])
     return (
-        <div>
-            <h1>Welcome to Rick and Morty Wiki</h1>
-            <p>This is a simple app that allows you to search for characters and episodes from the Rick and Morty series.</p>
-            <Link to="/search">Search</Link>
+        <div className="app__home">
+            <img src="./images/rick_morty.png" alt="rick and morty" className="app__home-photo" />
+            <h1 className="app__home-welcome">Welcome to Rick and Morty Wiki</h1>
+            <p className="app__home-description">This is a simple app that allows you to search for characters and episodes from the Rick and Morty series.</p>
+            <Link to="/search" style={{ textDecoration: 'none' }}><p className="app__home-link">👉 Let's Start 👈</p></Link>
         </div>
     )
 }
--- src/componenets/SelectEpisode.js
+++ src/componenets/SelectEpisode.js
@@ -2,19 +2,22 @@ import React from 'react'
 import uniqid from 'uniqid'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-
+import "./SelectEpisode.css"
 function SelectEpisode(props) {
     return (
-        <>
-            <h1>Select Episode</h1>
+        <div className="app__selectEpisode">
+            <h1 className="app__selectEpisode-title">Select Episode</h1>
             <ul>
                 {props.episodes?.map(episode => (
-                    <li key={uniqid()}>
-                        <Link to={`/search/${episode.id}`}>{episode.name}</Link>
+                    <li key={uniqid()} className="app__selectedEpisode-list">
+                        <Link to={`/search/${episode.id}`}>
+                            <h2 className="app__selectedEpisode-list-title">{episode.name}</h2>
+                            <p className="app__selectedEpisode-list-description">{episode.episode}</p>
+                        </Link>
                     </li>
                 ))}
             </ul>
-        </>
+        </div>
     )
 }
 const mapStateToProps = (state) => {
